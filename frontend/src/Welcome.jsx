0488ea8780f63214132158ec5a5825e634d8e686
JWin23 <iamkaungthantwin@gmail.com>
2023-10-04 01:36:01 -0500

    added welcome.jsx for the new game, join game, etc buttons

---
+++ frontend/src/Welcome.jsx
@@ -0,0 +1,12 @@
+import "./Welcome.css";
+
+export default function welcome() {
+
+  return (
+    <div className="welcome-container">
+      <button>New Game</button>
+      <button>Load Game</button>
+      <button>Join Game</button>
+    </div>
+  );
+};
