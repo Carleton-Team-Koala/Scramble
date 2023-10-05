0f3e4e46224099b2c58a6edeec84321ca1691f06
Roo Case <roo@roocase.org>
2023-10-04 21:28:54 -0500

    support for scores

--- languages/wordCheck.go
+++ languages/wordCheck.go
@@ -1,32 +1,57 @@
 // Special thanks to this package for some inspiration: https://pkg.go.dev/gitlab.com/ffe4/exercism-go/scrabble-score#section-readme
 
-package language
+package main
+
 import (
-  "fmt"
-  "os"
-  "io"
+    "encoding/json"
+    "fmt"
+    "os"
+    "net/http"
 )
 
-type alphabet struct{
-  listOfWords = make(map[string]int)
-  }
-
-type word struct{
-  spelling string
-  score int
+type alphabet struct {
+    ListOfWords map[string]int `json:"listOfWords"`
 }
 
 var activeAlphabet = new(alphabet)
 
-func parseAlphabet string{
-  jsonFile, err := os.Open("englishAlphabet.json")
-  if err != nil {
-    fmt.Println(err)
-  } else {
-    alphabet.listOfWords[]
-  }
+func letterReturner(w http.ResponseWriter, r *http.Request) {
+  // recreate json dataset of active alphabet
+  jsonData, err := json.Marshal(activeAlphabet)
+    if err != nil {
+        // Handle the error if marshaling fails
+        http.Error(w, "Unable to marshal JSON", http.StatusInternalServerError)
+        return
+    }
 
-  defer jsonFile.Close()
+  w.Header().Set("Content-Type", "application/json")
+  w.WriteHeader(http.StatusOK)
+  w.Write(jsonData)
 }
 
-var path_to_dict = string
+func importJSONdata(){
+   // Open the JSON file
+   fmt.Println("Opening file...")
+   file, err := os.Open("englishAlphabet.json")
+   if err != nil {
+       fmt.Println("Error opening file:", err)
+       return
+   }
+   defer file.Close()
+ 
+   // Create a decoder to read the JSON data
+   decoder := json.NewDecoder(file)
+ 
+   // Decode the JSON data into the struct
+   err = decoder.Decode(&activeAlphabet)
+   if err != nil {
+       fmt.Println("Error decoding JSON:", err)
+       return
+   }
+}
+
+func main() {
+  importJSONdata()
+  http.HandleFunc("/letterScores", letterReturner)
+  http.ListenAndServe(":8080", nil)
+}
