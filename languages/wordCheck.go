bea7ae618fee789df6e024e79aa96949f3dc0579
Roo Case <roo@roocase.org>
2023-10-06 00:23:21 -0500

    Added support for word checking

--- languages/wordCheck.go
+++ languages/wordCheck.go
@@ -3,27 +3,31 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
+
+	"github.com/gorilla/mux"
 )
+
+var scores = new(alphabet)
+var distribution = new(alphabet)
+var wordList = new(dictionary)
 
 // Alphabet datatype - stores an alphabet and its associated
 type alphabet struct {
 	ListOfWords map[string]int `json:"listOfWords"`
 }
 
-// TODO: simplify to make this one alphabet system
-// alphabetScoresFilePath and alphabetDistributionFilePath each represent the scores per letter/tile, and how many tiles of each letter/blank characters there are in this alphabet.
-var scores = new(alphabet)
-var distribution = new(alphabet)
-var alphabetScoresFilePath = "englishAlphabetScores.json"
-var alphabetDistributionFilePath = "englishAlphabetDistribution.json"
+type dictionary struct {
+	wordList []string
+}
 
 // getter
 func letterScores(w http.ResponseWriter, r *http.Request) {
-	fmt.Println
 	alphabetReturner(w, r, scores)
 }
 
@@ -67,14 +71,91 @@ func importJSONdata(path string, activeAlphabet *alphabet) {
 	}
 }
 
+func importDict(textPath string, words *dictionary) {
+	file, err := os.Open(textPath)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		word := scanner.Text()
+		wordList.wordList = append(wordList.wordList, word)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error opening file:", err)
+	}
+}
+
+func checkLetter(searchWord string) bool {
+	searchWord = strings.ToLower(searchWord)
+	left, right := 0, len(wordList.wordList)-1
+
+	for left <= right {
+		mid := left + (right-left)/2
+		midWord := strings.ToLower(wordList.wordList[mid])
+
+		if midWord == searchWord {
+			return true
+		} else if midWord < searchWord {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return false
+}
+
+func wordCheckInterface(w http.ResponseWriter, r *http.Request) {
+	jsonObject := make(map[string]interface{})
+
+	w.Header().Set("Content-Type", "application/json") //approves response
+	w.WriteHeader(http.StatusOK)                       //good HTTP response
+	word := mux.Vars(r)["word"]
+
+	if checkLetter(word) {
+		jsonObject["result"] = "true"
+	} else {
+		jsonObject["result"] = "false"
+	}
+	jsonData, err := json.Marshal(jsonObject)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	w.Write(jsonData)
+}
+
 func main() {
+
+	var alphabetScoresFilePath = "englishAlphabetScores.json"
+	var alphabetDistributionFilePath = "englishAlphabetDistribution.json"
+	var dictionaryText = "englishWordList.txt"
+
 	importJSONdata(alphabetScoresFilePath, scores)
 	importJSONdata(alphabetDistributionFilePath, distribution)
+	importDict(dictionaryText, wordList)
 
-	//http listener paths
-	http.HandleFunc("/letterScores", letterScores)
-	http.HandleFunc("/letterDistribution", letterDistribution)
+	r := mux.NewRouter()
+	r.HandleFunc("/checkWord/{word}", wordCheckInterface).Methods("GET")
 
-	//run server
-	http.ListenAndServe(":8080", nil)
+	// TODO: Import data and other setup code here
+
+	// Define additional routes for your application
+	r.HandleFunc("/letterScores", letterScores)
+	r.HandleFunc("/letterDistribution", letterDistribution)
+
+	http.Handle("/", r)
+
+	fmt.Println("Server is running on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
