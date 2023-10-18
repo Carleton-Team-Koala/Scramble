7dd7cfc4474b828bf32326b350057f1f9dcecddc
shkim2001 <tjdgmlkim19@gmail.com>
2023-10-18 18:32:30 -0500

    Modify languages api endpoint and set up get request for backend

--- app/backend/pkg/models/struct.go
+++ app/backend/pkg/models/struct.go
@@ -5,6 +5,7 @@ var GameList map[string]Game
 type Game struct {
 	GameID  string         `json:"GameID"`
 	Board   [15][15]string `json:"Board"`
+	AvailableLetters map[string]int `json:"LetterDistribution"`
 	Players []Player       `json:"Players"`
 }
 
