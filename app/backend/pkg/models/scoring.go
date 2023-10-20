2a19270867f6aa19f2f34c44d6be6ac7925ef188
Roo Case <roo@roocase.org>
2023-10-19 21:34:22 -0500

    added check in directions

--- app/backend/pkg/models/scoring.go
+++ app/backend/pkg/models/scoring.go
@@ -4,64 +4,54 @@ func scoring(activeGame Game, newTiles string) {
 	var word = ""
 	for i := 0; i < len(newTiles); i++ {
 		word = word + string(newTiles[i])
-
 	}
 }
 
-func checkLeft(x int, y int, activeGame Game, newTiles string, wordSoFar string, scoreValue int) WordScore {
+func endOfWord(scoreValue int) WordScore {
 	var foo WordScore
-	if x >= 0 {
+	// TODO: connect to langauges api for scoring/ word validation
+	var validWord bool
+	if validWord {
+		foo = WordScore{
+			Valid: true,
+			Score: scoreValue + 0,
+		}
+	} else {
+		foo = WordScore{
+			Valid: false,
+			Score: 0,
+		}
+	}
+	return foo
+
+}
+
+func checkLeft(x int, y int, activeGame Game, newTiles string, wordSoFar string, scoreValue int) WordScore {
+	if x > 0 {
 		if activeGame.Board[x-1][y] != "" {
+			//TODO - score individual tile to add to scoreValue
 			return checkLeft(x-1, y, activeGame, newTiles, activeGame.Board[x-1][y]+wordSoFar, scoreValue)
 		}
-		// TODO: connect to langauges api for scoring/ word validation
-		var validWord bool
-		if validWord {
-			foo = WordScore{
-				Valid: true,
-				Score: scoreValue + 0,
-			}
-		} else {
-			foo = WordScore{
-				Valid: false,
-				Score: scoreValue + 0,
-			}
-		}
-		return foo
-
+		return endOfWord(scoreValue)
 	}
-	foo = WordScore{
+
+	var foo = WordScore{
 		Valid: true,
 		Score: 0,
 	}
 	return foo
 }
 
-/*
-
 func checkRight(x int, y int, activeGame Game, newTiles string, wordSoFar string, scoreValue int) WordScore {
-	if x > 0 {
-		if activeGame.Board[x+1][y] != ""{
-			return checkLeft(x+1, y, activactiveGame, newTiles, activeGame.Board[x-1][y] + wordSoFar, scoreValue)
+	if x < 16 {
+		if activeGame.Board[x+1][y] != "" {
+			//TODO - score individual tile to add to scoreValue
+			return checkLeft(x-1, y, activeGame, newTiles, activeGame.Board[x-1][y]+wordSoFar, scoreValue)
 		}
-
-			// TODO: connect to langauges api for scoring/ word validation
-			var validWord bool
-			if validWord{
-				foo := WordScore{
-					Valid: true,
-					Score: scoreValue + 0,
-				}
-			} else {
-				foo := WordScore{
-					Valid: false,
-					Score: scoreValue + 0,
-				}
-			}
-			return foo
-
+		return endOfWord(scoreValue)
 	}
-	foo := WordScore{
+
+	var foo = WordScore{
 		Valid: true,
 		Score: 0,
 	}
@@ -70,56 +60,32 @@ func checkRight(x int, y int, activeGame Game, newTiles string, wordSoFar string
 
 func checkUp(x int, y int, activeGame Game, newTiles string, wordSoFar string, scoreValue int) WordScore {
 	if y > 0 {
-		if activeGame.Board[x][y-1] != ""{
-			return checkLeft(x, y-1, activactiveGame, newTiles, activeGame.Board[x-1][y] + wordSoFar, scoreValue)
+		if activeGame.Board[x][y-1] != "" {
+			//TODO - score individual tile to add to scoreValue
+			return checkLeft(x-1, y, activeGame, newTiles, activeGame.Board[x-1][y]+wordSoFar, scoreValue)
 		}
-
-			// TODO: connect to langauges api for scoring/ word validation
-			var validWord bool
-			if validWord{
-				foo := WordScore{
-					Valid: true,
-					Score: scoreValue + 0,
-				}
-			} else {
-				foo := WordScore{
-					Valid: false,
-					Score: scoreValue + 0,
-				}
-			}
-			return foo
-
+		return endOfWord(scoreValue)
 	}
-}
 
-
-func checkDown(x int, y int, activeGame Game, newTiles string, wordSoFar string, scoreValue int) WordScore {
-	if y > 0 {
-		if activeGame.Board[x][y+1] != ""{
-			return checkLeft(x. y+1, activactiveGame, newTiles, activeGame.Board[x-1][y] + wordSoFar, scoreValue)
-		}
-			// TODO: connect to langauges api for scoring/ word validation
-			var validWord bool
-			if validWord{
-				foo := WordScore{
-					Valid: true,
-					Score: scoreValue + 0,
-				}
-			} else {
-				foo := WordScore{
-					Valid: false,
-					Score: scoreValue + 0,
-				}
-			}
-			return foo
-
-	}
-	foo := WordScore{
+	var foo = WordScore{
 		Valid: true,
 		Score: 0,
 	}
 	return foo
 }
 
+func checkDown(x int, y int, activeGame Game, newTiles string, wordSoFar string, scoreValue int) WordScore {
+	if y < 15 {
+		if activeGame.Board[x][y+1] != "" {
+			//TODO - score individual tile to add to scoreValue
+			return checkLeft(x-1, y, activeGame, newTiles, activeGame.Board[x-1][y]+wordSoFar, scoreValue)
+		}
+		return endOfWord(scoreValue)
+	}
 
-*/
+	var foo = WordScore{
+		Valid: true,
+		Score: 0,
+	}
+	return foo
+}
