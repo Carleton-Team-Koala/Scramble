85033ab9ea5445d858cf93cfbf50492175720d7e
Roo Case <roo@roocase.org>
2023-10-30 18:33:32 -0500

    Begiinng scroing collections

--- app/backend/pkg/models/scoring.go
+++ app/backend/pkg/models/scoring.go
@@ -1,10 +1,59 @@
 package models
 
+import "container/list"
+
 func scoring(activeGame Game, newTiles string) {
-	var word = ""
+	score := 0
 	for i := 0; i < len(newTiles); i++ {
-		word = word + string(newTiles[i])
+		word = word + string(newTiles[i].letter)
+
+		var x = int(newTiles.x)
+		var y = int(newTiles.y)
+
+		var boolXDir = checkTwoDirectionsX(activeGame, x, y)
+		var boolYDir = checkTwoDirectionsX(activeGame, x, y)
+
+		if boolXDir {
+			//TODO: FILL
+		} else {
+			score += checkUp(x, y, activeGame, newTiles, "", 0)
+			score += checkDown(x, y, activeGame, newTiles, "", 0)
+		}
+
+		if boolYDir {
+			//TODO: FILL
+		} else {
+			score += checkUp(x, y, activeGame, newTiles, "", 0)
+			score += checkDown(x, y, activeGame, newTiles, "", 0)
+		}
+
+
+		return score
+
 	}
+
+	// check to see if word is valid
+
+}
+
+func checkTwoDirectionsX(activeGame Game, x int y int) tf bool{
+	if (x <= 0 || x >= 15){
+		return false
+	}
+	if (activeGame.Board[x-1][y] != nil && activeGame.Board[x+1][y] != nil){
+		return true
+	}
+	return false
+}
+
+func checkTwoDirectionsY(activeGame Game, x int y int) tf bool{
+	if (y <= 0 || y >= 15){
+		return false
+	}
+	if (activeGame.Board[x][y-1] != nil && activeGame.Board[x][y+1] != nil){
+		return true
+	}
+	return false
 }
 
 func endOfWord(scoreValue int) WordScore {
@@ -26,7 +75,7 @@ func endOfWord(scoreValue int) WordScore {
 
 }
 
-func checkLeft(x int, y int, activeGame Game, newTiles string, wordSoFar string, scoreValue int) WordScore {
+func checkLeft(x int, y int, activeGame Game, newTiles string, wordSoFar string, scoreValue int) WordScore int {
 	if x > 0 {
 		if activeGame.Board[x-1][y] != "" {
 			//TODO - score individual tile to add to scoreValue
@@ -42,7 +91,7 @@ func checkLeft(x int, y int, activeGame Game, newTiles string, wordSoFar string,
 	return foo
 }
 
-func checkRight(x int, y int, activeGame Game, newTiles string, wordSoFar string, scoreValue int) WordScore {
+func checkRight(x int, y int, activeGame Game, newTiles string, wordSoFar string, scoreValue int) WordScore  int{
 	if x < 16 {
 		if activeGame.Board[x+1][y] != "" {
 			//TODO - score individual tile to add to scoreValue
@@ -58,7 +107,7 @@ func checkRight(x int, y int, activeGame Game, newTiles string, wordSoFar string
 	return foo
 }
 
-func checkUp(x int, y int, activeGame Game, newTiles string, wordSoFar string, scoreValue int) WordScore {
+func checkUp(x int, y int, activeGame Game, newTiles string, wordSoFar string, scoreValue int) WordScore int{
 	if y > 0 {
 		if activeGame.Board[x][y-1] != "" {
 			//TODO - score individual tile to add to scoreValue
@@ -74,7 +123,7 @@ func checkUp(x int, y int, activeGame Game, newTiles string, wordSoFar string, s
 	return foo
 }
 
-func checkDown(x int, y int, activeGame Game, newTiles string, wordSoFar string, scoreValue int) WordScore {
+func checkDown(x int, y int, activeGame Game, newTiles string, wordSoFar string, scoreValue int) WordScore int {
 	if y < 15 {
 		if activeGame.Board[x][y+1] != "" {
 			//TODO - score individual tile to add to scoreValue
