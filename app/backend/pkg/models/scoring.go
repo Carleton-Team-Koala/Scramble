664d845dd116a70cba634a26e00463f413076dce
shkim2001 <tjdgmlkim19@gmail.com>
2023-11-01 16:30:31 -0500

    Scoring feature db connection

--- app/backend/pkg/models/scoring.go
+++ app/backend/pkg/models/scoring.go
@@ -1,199 +1,203 @@
 package models
 
-// import (
-// 	"errors"
-// )
+import (
+	"errors"
+)
 
-// func scoring(activeGame Game, newTiles string) (int, error) {
-// 	score := 0
-// 	word := ""
-// 	for i := 0; i < len(newTiles); i++ {
-// 		word = word + string(newTiles[i].letter)
+func (c *LanguageClient) scoring(activeGame Game, newTiles []Move) (int, error) {
+	score := 0
+	word, wordTwo, wordThree := "", "", ""
+	up, down, left, right := WordScore{}, WordScore{}, WordScore{}, WordScore{}
 
-// 		score += GetLetterScore(newTiles[i].letter)
+	for i := 0; i < len(newTiles); i++ {
+		word = word + newTiles[i].Letter
 
-// 		var x = int(newTiles.x)
-// 		var y = int(newTiles.y)
+		score += c.GetLetterScore(newTiles[i].Letter)
 
-// 		var boolXDir = checkTwoDirectionsX(activeGame, x, y)
-// 		var boolYDir = checkTwoDirectionsX(activeGame, x, y)
+		var x = int(newTiles[i].XLoc)
+		var y = int(newTiles[i].YLoc)
 
-// 		if boolXDir {
-// 			wordTwo := pullLeft(activeGame, x, y) + game.Board[x][y] + pullRight(activeGame, x, y)
-// 		} else {
-// 			up := checkUp(x, y, activeGame, newTiles, "", 0)
-// 			down := checkDown(x, y, activeGame, newTiles, "", 0)
+		var boolXDir = checkTwoDirectionsX(activeGame, x, y)
+		var boolYDir = checkTwoDirectionsX(activeGame, x, y)
 
-// 			if up.Valid {
-// 				score += up.Score
-// 			}
-// 			if down.Valid {
-// 				score += down.Score
-// 			}
+		if boolXDir {
+			wordTwo = pullLeft(activeGame, x, y) + activeGame.Board[x][y] + pullRight(activeGame, x, y)
+		} else {
+			up = c.checkUp(x, y, activeGame, newTiles[i].Letter, "", 0)
+			down = c.checkDown(x, y, activeGame, newTiles[i].Letter, "", 0)
 
-// 			if !up.Valid || !down.Valid {
-// 				return 0, errors.New("Invalid Words up/down")
-// 			}
-// 		}
+			if up.Valid {
+				score += up.Score
+			}
+			if down.Valid {
+				score += down.Score
+			}
 
-// 		if boolYDir {
-// 			wordThree := pullUp(activeGame, x, y) + game.Board[x][y] + pullDown(activeGame, x, y)
-// 		} else {
-// 			left := checkLeft(x, y, activeGame, newTiles, "", 0)
-// 			right := checkRight(x, y, activeGame, newTiles, "", 0)
+			if !up.Valid || !down.Valid {
+				return 0, errors.New("Invalid Words up/down")
+			}
+		}
 
-// 			if left.Valid {
-// 				score += up.Score
-// 			}
-// 			if right.Valid {
-// 				score += down.Score
-// 			}
+		if boolYDir {
+			wordThree = pullUp(activeGame, x, y) + activeGame.Board[x][y] + pullDown(activeGame, x, y)
+		} else {
+			left = c.checkLeft(x, y, activeGame, newTiles[i].Letter, "", 0)
+			right = c.checkRight(x, y, activeGame, newTiles[i].Letter, "", 0)
 
-// 			if !right.Valid || !left.Valid {
-// 				return 0, errors.New("Invalid Words left/right")
-// 			}
-// 		}
+			if left.Valid {
+				score += up.Score
+			}
+			if right.Valid {
+				score += down.Score
+			}
 
-// 	}
+			if !right.Valid || !left.Valid {
+				return 0, errors.New("Invalid Words left/right")
+			}
+		}
 
-// 	if CheckValidWord(word) {
-// 		if CheckValidWord(wordTwo) || wordTwo == "" {
-// 			if CheckValidWord(wordThree) || wordThree == "" {
-// 				return score, nil
-// 			}
-// 			return 0, errors.New("Invalid Words:" + wordThree)
-// 		}
-// 		return 0, errors.New("Invalid Words:" + wordTwo)
-// 	}
+	}
 
-// }
+	if c.CheckValidWord(word) {
+		if c.CheckValidWord(wordTwo) || wordTwo == "" {
+			if c.CheckValidWord(wordThree) || wordThree == "" {
+				return score, nil
+			}
+			return 0, errors.New("Invalid Words:" + wordThree)
+		}
+		return 0, errors.New("Invalid Words:" + wordTwo)
+	}
 
-// func pullUp(game Game, x int, y int) string {
-// 	if game.Board[x][y-1] == "" {
-// 		return ""
-// 	}
-// 	return pullUp(game, x, y-1) + game.Board[x][y-1]
-// }
 
-// func pullDown(game Game, x int, y int) string {
-// 	if game.Board[x][y+1] == "" {
-// 		return ""
-// 	}
-// 	return game.Board[x][y+1] + pullDown(game, x, y+1)
-// }
+	return 0, errors.New("Score not calculated")
+}
 
-// func pullLeft(game Game, x int, y int) string {
-// 	if game.Board[x-1][y] == "" {
-// 		return ""
-// 	}
-// 	return pullUp(game, x-1, y) + game.Board[x-1][y]
-// }
+func pullUp(game Game, x int, y int) string {
+	if game.Board[x][y-1] == "" {
+		return ""
+	}
+	return pullUp(game, x, y-1) + game.Board[x][y-1]
+}
 
-// func pullRight(game Game, x int, y int) string {
-// 	if game.Board[x+1][y] == "" {
-// 		return ""
-// 	}
-// 	return game.Board[x+1][y] + pullDown(game, x+1, y)
-// }
+func pullDown(game Game, x int, y int) string {
+	if game.Board[x][y+1] == "" {
+		return ""
+	}
+	return game.Board[x][y+1] + pullDown(game, x, y+1)
+}
 
-// func checkTwoDirectionsX(activeGame Game, x int, y int) bool {
-// 	if x <= 0 || x >= 15 {
-// 		return false
-// 	}
-// 	if activeGame.Board[x-1][y] != nil && activeGame.Board[x+1][y] != nil {
-// 		return true
-// 	}
-// 	return false
-// }
+func pullLeft(game Game, x int, y int) string {
+	if game.Board[x-1][y] == "" {
+		return ""
+	}
+	return pullUp(game, x-1, y) + game.Board[x-1][y]
+}
 
-// func checkTwoDirectionsY(activeGame Game, x int, y int) bool {
-// 	if y <= 0 || y >= 15 {
-// 		return false
-// 	}
-// 	if activeGame.Board[x][y-1] != "" && activeGame.Board[x][y+1] != "" {
-// 		return true
-// 	}
-// 	return false
-// }
+func pullRight(game Game, x int, y int) string {
+	if game.Board[x+1][y] == "" {
+		return ""
+	}
+	return game.Board[x+1][y] + pullDown(game, x+1, y)
+}
 
-// func endOfWord(scoreValue int, word string) WordScore {
-// 	var foo WordScore
-// 	var validWord = CheckValidWord(word)
-// 	if validWord {
-// 		foo = WordScore{
-// 			Valid: true,
-// 			Score: scoreValue + 0,
-// 		}
-// 	} else {
-// 		foo = WordScore{
-// 			Valid: false,
-// 			Score: 0,
-// 		}
-// 	}
-// 	return foo
+func checkTwoDirectionsX(activeGame Game, x int, y int) bool {
+	if x <= 0 || x >= 15 {
+		return false
+	}
+	if activeGame.Board[x-1][y] != "" && activeGame.Board[x+1][y] != "" {
+		return true
+	}
+	return false
+}
 
-// }
+func checkTwoDirectionsY(activeGame Game, x int, y int) bool {
+	if y <= 0 || y >= 15 {
+		return false
+	}
+	if activeGame.Board[x][y-1] != "" && activeGame.Board[x][y+1] != "" {
+		return true
+	}
+	return false
+}
 
-// func checkLeft(x int, y int, activeGame Game, newTiles string, wordSoFar string, scoreValue int) WordScore {
-// 	if x > 0 {
-// 		if activeGame.Board[x-1][y] != "" {
-// 			newScore := scoreValue + GetLetterScore(activeGame.Board[x-1][y])
-// 			return checkLeft(x-1, y, activeGame, newTiles, activeGame.Board[x-1][y]+wordSoFar, newScore)
-// 		}
-// 		return endOfWord(scoreValue, activeGame.Board[x-1][y]+wordSoFar)
-// 	}
+func (c *LanguageClient) endOfWord(scoreValue int, word string) WordScore {
+	var foo WordScore
+	var validWord = c.CheckValidWord(word)
+	if validWord {
+		foo = WordScore{
+			Valid: true,
+			Score: scoreValue + 0,
+		}
+	} else {
+		foo = WordScore{
+			Valid: false,
+			Score: 0,
+		}
+	}
+	return foo
 
-// 	var foo = WordScore{
-// 		Valid: true,
-// 		Score: 0,
-// 	}
-// 	return foo
-// }
+}
 
-// func checkRight(x int, y int, activeGame Game, newTiles string, wordSoFar string, scoreValue int) WordScore {
-// 	if x < 16 {
-// 		if activeGame.Board[x+1][y] != "" {
-// 			newScore := scoreValue + GetLetterScore(activeGame.Board[x+1][y])
-// 			return checkLeft(x-1, y, activeGame, newTiles, activeGame.Board[x+1][y]+wordSoFar, newScore)
-// 		}
-// 		return endOfWord(scoreValue, activeGame.Board[x+1][y]+wordSoFar)
-// 	}
+func (c *LanguageClient) checkLeft(x int, y int, activeGame Game, newTiles string, wordSoFar string, scoreValue int) WordScore {
+	if x > 0 {
+		if activeGame.Board[x-1][y] != "" {
+			newScore := scoreValue + c.GetLetterScore(activeGame.Board[x-1][y])
+			return c.checkLeft(x-1, y, activeGame, newTiles, activeGame.Board[x-1][y]+wordSoFar, newScore)
+		}
+		return c.endOfWord(scoreValue, activeGame.Board[x-1][y]+wordSoFar)
+	}
 
-// 	var foo = WordScore{
-// 		Valid: true,
-// 		Score: 0,
-// 	}
-// 	return foo
-// }
+	var foo = WordScore{
+		Valid: true,
+		Score: 0,
+	}
+	return foo
+}
 
-// func checkUp(x int, y int, activeGame Game, newTiles string, wordSoFar string, scoreValue int) WordScore {
-// 	if y > 0 {
-// 		if activeGame.Board[x][y-1] != "" {
-// 			newScore := scoreValue + GetLetterScore(activeGame.Board[x][y-1])
-// 			return checkLeft(x-1, y, activeGame, newTiles, activeGame.Board[x][y-1]+wordSoFar, newScore)
-// 		}
-// 		return endOfWord(scoreValue, activeGame.Board[x][y-1]+wordSoFar)
-// 	}
+func (c *LanguageClient) checkRight(x int, y int, activeGame Game, newTiles string, wordSoFar string, scoreValue int) WordScore {
+	if x < 16 {
+		if activeGame.Board[x+1][y] != "" {
+			newScore := scoreValue + c.GetLetterScore(activeGame.Board[x+1][y])
+			return c.checkLeft(x-1, y, activeGame, newTiles, activeGame.Board[x+1][y]+wordSoFar, newScore)
+		}
+		return c.endOfWord(scoreValue, activeGame.Board[x+1][y]+wordSoFar)
+	}
 
-// 	var foo = WordScore{
-// 		Valid: true,
-// 		Score: 0,
-// 	}
-// 	return foo
-// }
+	var foo = WordScore{
+		Valid: true,
+		Score: 0,
+	}
+	return foo
+}
 
-// func checkDown(x int, y int, activeGame Game, newTiles string, wordSoFar string, scoreValue int) WordScore {
-// 	if y < 15 {
-// 		if activeGame.Board[x][y+1] != "" {
-// 			newScore := scoreValue + GetLetterScore(activeGame.Board[x][y+1])
-// 			return checkLeft(x-1, y, activeGame, newTiles, activeGame.Board[x][y+1]+wordSoFar, newScore)
-// 		}
-// 		return endOfWord(scoreValue, activeGame.Board[x][y+1]+wordSoFar)
-// 	}
+func (c *LanguageClient) checkUp(x int, y int, activeGame Game, newTiles string, wordSoFar string, scoreValue int) WordScore {
+	if y > 0 {
+		if activeGame.Board[x][y-1] != "" {
+			newScore := scoreValue + c.GetLetterScore(activeGame.Board[x][y-1])
+			return c.checkLeft(x-1, y, activeGame, newTiles, activeGame.Board[x][y-1]+wordSoFar, newScore)
+		}
+		return c.endOfWord(scoreValue, activeGame.Board[x][y-1]+wordSoFar)
+	}
 
-// 	var foo = WordScore{
-// 		Valid: true,
-// 		Score: 0,
-// 	}
-// 	return foo
-// }
+	var foo = WordScore{
+		Valid: true,
+		Score: 0,
+	}
+	return foo
+}
+
+func (c *LanguageClient) checkDown(x int, y int, activeGame Game, newTiles string, wordSoFar string, scoreValue int) WordScore {
+	if y < 15 {
+		if activeGame.Board[x][y+1] != "" {
+			newScore := scoreValue + c.GetLetterScore(activeGame.Board[x][y+1])
+			return c.checkLeft(x-1, y, activeGame, newTiles, activeGame.Board[x][y+1]+wordSoFar, newScore)
+		}
+		return c.endOfWord(scoreValue, activeGame.Board[x][y+1]+wordSoFar)
+	}
+
+	var foo = WordScore{
+		Valid: true,
+		Score: 0,
+	}
+	return foo
+}
