5aa0ec912825904d7d34c0d43793780e8c3c01be
Ben Zhao <zhaob@carleton.edu>
2023-11-13 11:58:07 -0600

    leaderboard

--- app/database/cmd/init.sql
+++ app/database/cmd/init.sql
@@ -21,3 +21,48 @@ GRANT ALL PRIVILEGES ON DATABASE scramble_db to KoalaAdmin;
 GRANT CONNECT ON DATABASE scramble_db TO ReadOnlyUser;
 GRANT USAGE ON SCHEMA public TO ReadOnlyUser;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO ReadOnlyUser;
+
+CREATE TABLE IF NOT EXISTS games (
+    GameID TEXT PRIMARY KEY,
+    Board JSONB NOT NULL,
+    LetterDistribution JSONB NOT NULL,
+    Player1ID TEXT REFERENCES players(PlayerID),
+    Player1Score INT,
+    Player2ID TEXT REFERENCES players(PlayerID),
+    Player2Score INT
+);
+
+ALTER TABLE games
+ADD COLUMN IF NOT EXISTS PlayerScores JSONB;
+
+
+WITH PlayerScores AS (
+    SELECT
+        GameID,
+        (Players->0->>'name') AS Player1Name,
+        (Players->0->>'score')::INT AS Player1Score,
+        (Players->1->>'name') AS Player2Name,
+        (Players->1->>'score')::INT AS Player2Score
+    FROM games
+)
+
+SELECT
+    PlayerName,
+    SUM(PlayerScore) AS TotalScore
+FROM (
+    SELECT
+        GameID,
+        Player1Name AS PlayerName,
+        Player1Score AS PlayerScore
+    FROM PlayerScores
+
+    UNION ALL
+
+    SELECT
+        GameID,
+        Player2Name AS PlayerName,
+        Player2Score AS PlayerScore
+    FROM PlayerScores
+) AS AllPlayerScores
+GROUP BY PlayerName
+ORDER BY TotalScore DESC;
