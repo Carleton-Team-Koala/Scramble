41e4c44f48345db5c834e8a146fe2bd72d5e5bd4
Roo Case <roo@roocase.org>
2023-11-13 19:43:04 -0600

    Added DL support

--- app/backend/pkg/models/scoring.go
+++ app/backend/pkg/models/scoring.go
@@ -8,14 +8,18 @@ import (
 func (c *LanguageClient) scoring(activeGame Game, newTiles []Move) (int, error) {
 	score := 0
 	setOfWords := []string{}
-	scoreModifier := []string{}
+	scoreModifier := [15]string{}
+	OriginalWord := ""
+	scoreAggregateModifier := 1
+	OGWordScore := 0
 
 	for i := 0; i < len(newTiles); i++ {
 
 		var x = int(newTiles[i].Col)
 		var y = int(newTiles[i].Row)
 
-		scoreModifier[i] = checkForScoreModidier(x, y)
+		OriginalWord += string(newTiles[i].Letter)
+		scoreModifier[i] = checkForScoreModifier(x, y)
 
 		// recursively get all the possible words
 		leftAndRightWord := pullLeft(activeGame, x, y) + activeGame.Board[x][y] + pullRight(activeGame, x, y)
@@ -39,11 +43,31 @@ func (c *LanguageClient) scoring(activeGame Game, newTiles []Move) (int, error) 
 
 	for _, word := range setOfWords {
 		if c.CheckValidWord(word) {
-			for _, letter := range word {
-				score += c.GetLetterScore(string(letter))
+			if OriginalWord == word {
+				for i := 0; i < len(word); i++ {
+					switch scoreModifier[i] {
+					case "dl":
+						OGWordScore += 2 * (c.GetLetterScore(string(word[i])))
+					case "tl":
+						OGWordScore += 3 * (c.GetLetterScore(string(word[i])))
+					case "dw":
+						scoreAggregateModifier *= 2
+					case "tw":
+						scoreAggregateModifier *= 3
+					case "na":
+					default:
+						OGWordScore += (c.GetLetterScore(string(word[i])))
+					}
+				}
+			} else {
+				for _, letter := range word {
+					score += c.GetLetterScore(string(letter))
+				}
 			}
 		}
 	}
+
+	score += (scoreAggregateModifier * OGWordScore)
 
 	return score, nil
 }
@@ -102,7 +126,7 @@ func checkWordExists(setOfWords []string, word string) bool {
  This function checks if a given tile if a double word, triple word, double letter, or triple letter score.
 */
 
-func checkForScoreModidier(x int, y int) string {
+func checkForScoreModifier(x int, y int) string {
 	tw := [][2]int{{0, 0}, {0, 7}, {0, 14}, {7, 0}, {7, 14}, {14, 0}, {14, 7}, {14, 14}}
 	dw := [][2]int{{1, 1}, {2, 2}, {3, 3}, {4, 4}, {10, 10}, {11, 11}, {12, 12}, {13, 13}, {1, 13}, {2, 12}, {3, 11}, {4, 10}, {10, 4}, {11, 3}, {12, 2}, {13, 1}}
 	tl := [][2]int{{1, 5}, {1, 9}, {5, 1}, {5, 5}, {5, 9}, {5, 13}, {9, 1}, {9, 5}, {9, 9}, {9, 13}, {13, 5}, {13, 9}}
